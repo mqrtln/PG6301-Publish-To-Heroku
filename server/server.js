8aefc91686e6ad634cbd66c5d4e85e8bd2897f3f
Martin Olaussen <maol049@EGMS.no>
2022-02-11 13:49:25 +0100

    Root application definition

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const app = express();
+
+app.get("/login", (req, res, next) =>{
+    res.json({username: "Noen Andre"});
+})
+app.post("/login", (req, res, next) =>{
+    res.sendStatus(401);
+    
+})
+
+app.listen(3000);
