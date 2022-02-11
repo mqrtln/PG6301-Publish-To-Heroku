0f001e3ff4ebfb369b1d00a30faad66d0cf3ce9d
Martin Olaussen <maol049@EGMS.no>
2022-02-11 16:38:26 +0100

    use React Router in client (fixed with middleware)

--- client/index.jsx
+++ client/index.jsx
@@ -1,4 +1,45 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
-ReactDOM.render(<h1>Hello React</h1>, document.getElementById("app"));
+function FrontPage() {
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <ul>
+        <li>
+          <Link to={"/login"}>Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <form>
+      <h1>Login</h1>
+      <div>
+        Username: <input />
+      </div>
+      <div>
+        Password <input type="password" />
+      </div>
+      <button>Submit</button>
+    </form>
+  );
+}
+
+function Application() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<FrontPage />} />
+        <Route path={"/login"} element={<Login />} />
+        <Route path={"/*"} element={<h1>Not Found</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.render(<Application />, document.getElementById("app"));
--- server/server.js
+++ server/server.js
@@ -1,15 +1,20 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
-app.get("/login", (req, res, next) => {
-  res.json({ username: "Yo Tora" });
+app.get("/api/login", (req, res, next) => {
+  res.json({ username: "Noen Andre" });
 });
-app.post("/login", (req, res, next) => {
+app.post("/api/login", (req, res, next) => {
   res.sendStatus(401);
 });
 
 app.use(express.static("../client/dist"));
+
+app.use((req, res, next) => {
+  res.sendFile(path.resolve("../client/dist/index.html"));
+});
 
 const server = app.listen(process.env.PORT || 3000, () => {
   console.log(`Started on http://localhost:${server.address.port}`);
